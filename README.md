02122893f587460ff6fcc1d5129a4da452756427
Till Krüss <till@kruss.io>
2021-11-02 11:18:44 -0700

    add WC login example

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # k6
 
-We use these script to benchmark Object Cache Pro and Relay with various hosting partners.
+We use these scripts to benchmark Object Cache Pro and Relay with various hosting partners.
 
 ## Setup
 
@@ -14,7 +14,7 @@ Fetches all WordPress sitemaps and requests random URLs.
 
 ```
 k6 run wp.js
-k6 run --vus=100 --duration=10m wp.js
+k6 run wp.js --vus=100 --duration=10m
 ```
 
 ### `woo-checkout.js`
@@ -22,7 +22,18 @@ k6 run --vus=100 --duration=10m wp.js
 Loads the homepage, selects and loads a random category, selects a random product and adds it to the cart, loads the cart page and then places an order.
 
 ```
+wp option update woocommerce_enable_guest_checkout no --autoload=no
+wp option update woocommerce_enable_signup_and_login_from_checkout yes --autoload=no
+
 k6 run woo-checkout.js
+```
+
+### `woo-customer.js`
+
+Loads the homepage, signs in, views at orders and then their account details.
+
+```
+k6 run woo-customer.js
 ```
 
 ## Environment variables
@@ -32,7 +43,7 @@ k6 run woo-checkout.js
 You can pass in the `SITE_URL` to point the traffic at your own site.
 
 ```
-k6 run -e SITE_URL=http://localhost:8080 woo-ramping.js
+k6 run wp.js -e SITE_URL=http://localhost:8080
 ```
 
 ### Bypass page caches
@@ -40,7 +51,7 @@ k6 run -e SITE_URL=http://localhost:8080 woo-ramping.js
 To attempt bypassing page caches without logging in, pass in `BYPASS_CACHE`:
 
 ```
-k6 run -e BYPASS_CACHE=1 wp.js
+k6 run wp.js -e BYPASS_CACHE=1 
 ```
 
 ## Reset WooCommerce orders
---
+++ woo-customer.js
@@ -0,0 +1,132 @@
+import http from 'k6/http'
+import { check, group, fail, sleep } from 'k6'
+import { Rate, Trend } from 'k6/metrics'
+
+import { rand, wpMetrics, bypassPageCacheCookies } from './lib/helpers.js'
+import { isOK, pageIsNotLogin } from './lib/checks.js'
+
+export const options = {
+    throw: true,
+    scenarios: {
+        ramping: {
+            executor: 'ramping-vus',
+            startVUs: 1,
+            gracefulStop: '10s',
+            gracefulRampDown: '10s',
+            stages: [
+                { duration: '1m', target: 100 },
+            ],
+        },
+        // constant: {
+        //     executor: 'constant-vus',
+        //     vus: 100,
+        //     duration: '1m',
+        //     gracefulStop: '10s',
+        // },
+    },
+}
+
+const errorRate = new Rate('errors')
+
+// metrics provided by Object Cache Pro
+const cacheHits = new Trend('cache_hits')
+const storeReads = new Trend('store_reads')
+const storeWrites = new Trend('store_writes')
+const msCache = new Trend('ms_cache', true)
+const msCacheRatio = new Trend('ms_cache_ratio')
+
+export default function () {
+    let metrics
+
+    const jar = new http.CookieJar()
+    const siteUrl = __ENV.SITE_URL || 'https://test.cachewerk.com'
+
+    const pause = {
+        min: 3,
+        max: 8,
+    }
+
+    const addResponseMetrics = (response) => {
+        if (metrics = wpMetrics(response)) {
+            cacheHits.add(metrics.hits)
+            storeReads.add(metrics.storeReads)
+            storeWrites.add(metrics.storeWrites)
+            msCache.add(metrics.msCache)
+            msCacheRatio.add(metrics.msCacheRatio)
+        }
+    }
+
+    if (__ENV.BYPASS_CACHE) {
+        Object.entries(bypassPageCacheCookies()).forEach(([key, value]) => {
+            jar.set(siteUrl, key, value, { path: '/' })
+        })
+    }
+
+    group('Load homepage', function () {
+        const response = http.get(siteUrl, { jar })
+
+        check(response, { isOK })
+            || (errorRate.add(1) && fail('status code was *not* 200'))
+
+        addResponseMetrics(response)
+    })
+
+    sleep(rand(pause.min, pause.max))
+
+    group('Login', function () {
+        const response = http.get(`${siteUrl}/my-account/`, { jar })
+
+        check(response, { isOK })
+            || (errorRate.add(1) && fail('status code was *not* 200'))
+
+        addResponseMetrics(response)
+
+        const formResponse = response.submitForm({
+            formSelector: 'form.woocommerce-form-login',
+            params: { jar },
+            fields: {
+                // username: `test${rand(1, 10000)}`,
+                // password: '3405691582',
+                username: `Till`,
+                password: 'ab32p*W@x5ZKxU!3bp',
+                rememberme: 'forever',
+            },
+        })
+
+        check(formResponse, { isOK })
+            || (errorRate.add(1) && fail('status code was *not* 200'))
+
+        check(formResponse, { pageIsNotLogin })
+            || fail('page *has* login form')
+
+        addResponseMetrics(formResponse)
+    })
+
+    sleep(rand(pause.min, pause.max))
+
+    group('Load orders', function () {
+        const response = http.get(`${siteUrl}/my-account/orders/`, { jar })
+
+        check(response, { isOK })
+            || (errorRate.add(1) && fail('status code was *not* 200'))
+
+        check(response, { pageIsNotLogin })
+            || fail('page *has* login form')
+
+        addResponseMetrics(response)
+    })
+
+    sleep(rand(pause.min, pause.max))
+
+    group('Load orders', function () {
+        const response = http.get(`${siteUrl}/my-account/orders/`, { jar })
+
+        check(response, { isOK })
+            || (errorRate.add(1) && fail('status code was *not* 200'))
+
+        check(response, { pageIsNotLogin })
+            || fail('page *has* login form')
+
+        addResponseMetrics(response)
+    })
+}
