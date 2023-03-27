1dd8be6466997720366647c55a4c206c3851c832
Till Krüss <tillkruss@users.noreply.github.com>
2023-03-27 13:31:23 -0700

    skip out of stock products; fix typo

--- woo-checkout.js
+++ woo-checkout.js
@@ -88,7 +88,7 @@ export default function () {
 
         return response.html()
             .find('.products')
-            .find('.product:not(.product-type-variable)') // skip variable products
+            .find('.product:not(.product-type-variable,.outofstock)') // skip products
             .find('.woocommerce-loop-product__link')
             .map((idx, el) => el.attr('href'))
     })
@@ -147,7 +147,7 @@ export default function () {
 
     sleep(rand(pause.min, pause.max))
 
-    group('Place holder', function () {
+    group('Place order', function () {
         const response = http.get(`${siteUrl}/checkout`, { jar })
 
         check(response, isOK)
