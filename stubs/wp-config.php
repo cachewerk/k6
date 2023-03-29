9c0a3058340c59977f7e64be76f4257af361b07d
Till Krüss <tillkruss@users.noreply.github.com>
2023-03-28 17:19:09 -0700

    Create wp-config.php

---
+++ stubs/wp-config.php
@@ -0,0 +1,43 @@
+# Configuration examples
+
+## Object Cache Pro
+
+```php
+define('WP_REDIS_CONFIG', [
+    'token' => '...',
+    'host' => '127.0.0.1',
+    'port' => 6379,
+    'database' => 0,
+    // 'password' => null,
+    'timeout' => 0.5,
+    'read_timeout' => 0.5,
+    'retries' => 3,
+    'prefetch' => true,
+    'serializer' => 'igbinary',
+    'split_alloptions' => true,
+    'debug' => false,
+    'save_commands' => false,
+]);
+```
+
+## Object Cache Pro + Relay
+
+```php
+define('WP_REDIS_CONFIG', [
+    'token' => '...',
+    'host' => '127.0.0.1',
+    'port' =>  6379,
+    'database' => 0,
+    // 'password' => null,
+    'timeout' => 0.5,
+    'read_timeout' => 0.5,
+    'retries' => 3,
+    'client' => 'relay',
+    'prefetch' => false,
+    'serializer' => 'igbinary',
+    'compression' => 'zstd',
+    'split_alloptions' => false,
+    'debug' => false,
+    'save_commands' => false,
+]);
+```
